4dd90f379acc18ec90ffbdff2ab11f43bb344c7c
Bhavik Prajapati <69726429+rahul429-wq@users.noreply.github.com>
2021-10-01 09:00:56 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,7 +1,11 @@
 public class multiply {
 
     public static void main(String[] args) {
-        System.out.println("multiply");
+        System.out.println("multiply program");
+        int a=10;
+        int b=20;
+        int c=a*b;
+        System.out.println(c);
     }
     
 }
